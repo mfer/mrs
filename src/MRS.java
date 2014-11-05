3502cc42021f718553dcaef5fd1712e4b70c23d0
mfer <sudolshw@gmail.com>
2014-11-05 19:18:44 -0200

    more on the PG draft

--- src/MRS.java
+++ src/MRS.java
@@ -19,12 +19,18 @@ public class MRS{
   //x PDA(w, N^in_D, N^out_D, eps)
   // Price-Directive Algorithm
 
+  private static boolean IS(Set<Integer> C){
+    //return true if the set is independent, false otherwise
+    //implement this
+    return true;
+  }
+
   //I PG(w, A, x)
   // Prune-and-Grow algorithm
-  private static void PG(){
-    int i=1;
+  private static Set<Integer> PG(){
+    int i=0, a;
     ArrayList<Double> w = new ArrayList<Double>();
-    w.add(5.0);
+    w.add(0.0);
     w.add(1.0);
     w.add(2.0);
     w.add(3.0);
@@ -32,58 +38,67 @@ public class MRS{
     w.add(5.0);
     
     System.out.println("Prune-Phase");
-
-    //B ← A, S ← ∅; //S is a stack
-    Set<Integer> B = new TreeSet<Integer>();
-    B.addAll(Arrays.asList(1,2,3,4,5));
-    Stack S = new Stack();
-    
-    while(!B.isEmpty()){
-      System.out.println(B);
-
+      //B ← A, S ← ∅; //S is a stack
+      Set<Integer> B = new TreeSet<Integer>();
+      B.addAll(Arrays.asList(1,2,3,4,5));
+      Stack<Integer> S = new Stack<Integer>();
+      while(!B.isEmpty()){
+//      System.out.println(B);
         //a ← a x-surplus link of B;
-        int a = i;
-        System.out.println("a= "+a);
-
+        a = i;
         //B ← B ∖ {a};
-        B.removeAll(Arrays.asList(a));        
-
-        //w (a) ← w (a) − w (S ∩ N (a));
+        B.removeAll(Arrays.asList(a));
+        //w(a) ← w (a) − w (S ∩ N (a));
         int b = 0;
         w.set(a,w.get(a) - w.get(b));
-        System.out.println(i+" "+w.get(i));
-
-
-        //if w (a) > 0, push a onto S;   
-      
-      i++;
-    }
-
-    for(i = 0;i<w.size();i++) {
-         System.out.println("w: " + i);
-         System.out.println("    weight: " + w.get(i));
-    }    
+//        System.out.println(a+" "+w.get(a));
+        //if w(a) > 0, push a onto S;
+        if(w.get(a) > 0.0 ) S.push(new Integer(a));
+        i++;
+      }
 
     System.out.println("Grow-Phase");
+      //I ← ∅;
+      Set<Integer> I = new TreeSet<Integer>();    
+      //while S ∕= ∅,
+      while(!S.empty()){
+        //pop the top link a from S;
+        a = S.pop();
+        System.out.println("pop "+a);
+        
+        //if I ∪ {a} is independent, I ← I ∪ {a};
+        Set<Integer> Sa = new TreeSet<Integer>();
+        Sa.add(a);
+        Set<Integer> union = new TreeSet<Integer>();
+        union.addAll(I);
+        union.addAll(Sa);
+        if(IS(union)) {
+          I.clear();
+          I.addAll(union);
+        }
+      }
+
+    //return I.
+    return I;
   }
 
   public static void main(String[] args) {
     for(int i = 0;i<args.length;i++) {
-         System.out.println(args[i]);
+      System.out.println(args[i]);
     }
 
     ArrayList<Link> links = new ArrayList<Link>();
     links.add(new Link(0.0,0.0,2.0,0.0, 1.0));
     links.add(new Link(2.0,1.0,2.0,3.0, 1.0));
-
+/*
     System.out.println(links.size());
     for(int i = 0;i<links.size();i++) {
-         System.out.println("link: " + i);
-         System.out.println("    sender:   (" + links.get(i).x_sender + ", " + links.get(i).y_sender + ")");
-         System.out.println("    receiver: (" + links.get(i).x_receiver + ", " + links.get(i).y_receiver + ")");
-         System.out.println("    size: " + links.get(i).length);
+      System.out.println("link: " + i);
+      System.out.println("    sender:   (" + links.get(i).x_sender + ", " + links.get(i).y_sender + ")");
+      System.out.println("    receiver: (" + links.get(i).x_receiver + ", " + links.get(i).y_receiver + ")");
+      System.out.println("    size: " + links.get(i).length);
     }    
-
+*/
 
     System.out.println("link_into_graph");
 
@@ -96,6 +111,6 @@ public class MRS{
     System.out.println("PDA");
 
     System.out.println("PG");
-    PG();
+    System.out.println("I = " + PG());
   }
 }
