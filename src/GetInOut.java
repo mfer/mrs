c3ed38f28c9f7ddcb485c0ae47f8fda6d26f9043
haueisen <dop@Mac-Pro-de-Flavio.local>
2014-11-05 17:46:23 -0200

    get_in_out corrections

--- src/GetInOut.java
+++ src/GetInOut.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 //N^in_D,N^out_D get_in_out (D)
 // Establish for all links the in and out edges
 
@@ -8,12 +10,16 @@ public class GetInOut{
                        ArrayList<ArrayList<Integer>> Outs){
     // out neighbors are simply the adjacents in graph D
 
-    for(Link link : D.links)
+    Ins.clear();
+    Outs.clear();
+
+    for(Link link : D.links){
       int linkIndex = D.links.indexOf(link);
-      Outs.get(linkIndex).add(D.adjacencia.get(linkIndex));
+      Outs.add(D.adjacencia.get(linkIndex));
         for(Integer out : Outs.get(linkIndex)){
           //in neighbors of A are out neighbors of other links
           Ins.get(out).add(linkIndex);
         }
+    }
   }
 }
