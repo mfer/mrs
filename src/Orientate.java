d5cdde436abfc83be01d75489a17af6fb0ab16f3
haueisen <dop@Mac-Pro-de-Flavio.local>
2014-11-05 16:39:54 -0200

    orientate_edge, directional graph D from link-conflit graph G

---
+++ src/Orientate.java
@@ -0,0 +1,49 @@
+// D orientate_edge (G)
+// Assign an orientation on the conflict edges (needs more study on how to do)
+/*
+
+  c > 1
+  c --> constant
+  sender interference radius >= c * link length
+
+  consider links a and b
+
+  if a receiver in b sender inteference
+  orientation b-->a
+
+  ties are broken arbitrarily
+
+
+*/
+public class Orientate{
+
+  Grafo D;
+
+  static Grafo edge(Grafo G){
+    D = new Grafo(G.links.size(),false);
+    //for each node N in G
+    for(Link link : G.links){
+      //for each adjacent M of N
+      D.links.add(link);
+      int linkIndex = G.links.indexOf(link);
+      for(Integer adj : G.adjacencia.get(linkIndex)){
+
+        Link adjacente = G.links.get(adj);
+
+        //distancia entre o receiver de link e o sender do adjacente
+        double dist_ab = Math.sqrt(Math.abs(link.x_receiver - adjacente.x_sender) *
+                              Math.abs(link.x_receiver - adjacente.x_sender) +
+                              Math.abs(link.y_receiver - adjacente.y_sender) *
+                              Math.abs(link.y_receiver - adjacente.y_sender));
+        if(adjacente.inteference_radius >= dist){
+          // Direcao D de adjacente para link
+          D.adjacencia.get(adj).add(linkIndex);
+        }else{//estah em conflito, se a direcao nao eh b-->a, entao...
+          D.adjacencia.get(linkIndex).add(adj);
+        }
+      }
+    }
+    return D;
+  }
+
+}
