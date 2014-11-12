70970b9f684201d08e94d31e958fc585e0945f26
mfer <sudolshw@gmail.com>
2014-11-12 16:12:56 -0200

    PG and MRS seems to be unconsciously removed

---
+++ src/MRS.java
@@ -0,0 +1,167 @@
+import java.io.*;
+import java.util.*;
+
+public class MRS{
+
+  static HashMap<Double,Integer> dataRateHashB=new HashMap<Double,Integer>();
+  static HashMap<Double,Integer> dataRateHashN=new HashMap<Double,Integer>();
+
+  private static void createDataRateTableB(){
+    dataRateHashB.put(new Double(4), new Integer(1));
+    dataRateHashB.put(new Double(6), new Integer(2));
+    dataRateHashB.put(new Double(8), new Integer(6));
+    dataRateHashB.put(new Double(10), new Integer(11));
+  }
+  private static void createDataRateTableN(){
+    dataRateHashN.put(new Double(14), new Integer(30));
+    dataRateHashN.put(new Double(17), new Integer(60));
+    dataRateHashN.put(new Double(19), new Integer(90));
+    dataRateHashN.put(new Double(22), new Integer(120));
+    dataRateHashN.put(new Double(26), new Integer(180));
+    dataRateHashN.put(new Double(30), new Integer(240));
+    dataRateHashN.put(new Double(31), new Integer(270));
+    dataRateHashN.put(new Double(32), new Integer(300));
+  }
+
+  public static void main(String[] args) {
+    for(int i = 0;i<args.length;i++) {
+      System.out.println(args[i]);
+    }
+
+    ArrayList<Link> links = new ArrayList<Link>();
+
+
+    links.add(new Link(2.0, 2.0, 2.5, 1.5, 1.0));
+    links.add(new Link(3.5, 2.0, 3.5, 3.0, 1.0));
+    links.add(new Link(2.5, 1.0, 3.5, 1.5, 1.0));
+    links.add(new Link(0.8, 1.0, 2.0, 0.8, 1.0));
+    links.add(new Link(0.5, 2.0, 0.5, 3.0, 1.0));
+    links.add(new Link(1.5, 3.0, 1.5, 2.5, 1.0));
+    links.add(new Link(2.5, 3.0, 3.5, 2.5, 1.0));
+
+    links.add(new Link(2.0, 0.0, 2.0, 1.5, 1.0));
+    links.add(new Link(0.0, 1.0, 1.5, 0.5, 1.0));
+    links.add(new Link(4.0, 1.0, 3.0, 0.5, 1.0));
+    links.add(new Link(2.0, 2.0, 3.0, 1.2, 1.0));
+    links.add(new Link(0.0, 3.0, 0.5, 1.5, 1.0));
+    links.add(new Link(4.0, 3.0, 3.2, 3.8, 1.0));
+    links.add(new Link(1.0, 4.0, 1.5, 3.1, 1.0));
+    links.add(new Link(3.0, 4.0, 2.5, 3.1, 1.0));
+
+
+/*    
+    System.out.println(links.size());
+    for(int i = 0;i<links.size();i++) {
+      System.out.println("link: " + i);
+      System.out.println("    sender:   (" + links.get(i).x_sender + ", " + links.get(i).y_sender + ")");
+      System.out.println("    receiver: (" + links.get(i).x_receiver + ", " + links.get(i).y_receiver + ")");
+      System.out.println("    length: " + links.get(i).length);
+      System.out.println("    interference: " + links.get(i).interference_radius);
+    }    
+*/
+
+//__________________________________________________________________________________________________________
+    System.out.println("BEGIN");        
+//__________________________________________________________________________________________________________     
+
+  //w,DG_z link_into_graph (L)
+  // Construct the Disk Graph DGz and the weight w for the links using the data-rates
+  // definition-4.4 from olga:2012, using the set of links L
+    //implemented at GrafoRadial as a constructor
+    //and at Link as the set_weight method called by the constructor.
+    System.out.println("link_into_graph");
+    GrafoRadial DGz = new GrafoRadial(links);
+
+    //TODO: finish GrafoRadial.set_weight
+//__________________________________________________________________________________________________________
+
+  //G,A graph_into_protocol (DGz)
+  // Construct the conflict graph G and the vertex set A from DGz
+    Grafo G = new Grafo(links);
+    System.out.println("graph_into_protocol");
+//__________________________________________________________________________________________________________
+
+  //D orientate_edge (G)
+  // Assign an orientation on the conflict edges
+    Grafo D;
+    System.out.println("orientate_edge");
+    D = Orientate.edge(G);    
+
+/*
+    GrafoRadial.draw(D.links, true);
+    
+    for(ArrayList<Integer> link : D.adjacencia){
+      int linkIndex = D.adjacencia.indexOf(link);
+      for(int adj : link){
+        System.out.println(linkIndex+" <-- "+adj);
+      }
+    }
+*/
+//__________________________________________________________________________________________________________
+
+  //N^in_D,N^out_D get_in_out (D)
+  // Establish for all links the in and out edges
+    System.out.println("get_in_out");
+    ArrayList<ArrayList<Integer>> Ins = new ArrayList<ArrayList<Integer>>();
+    ArrayList<ArrayList<Integer>> Outs = new ArrayList<ArrayList<Integer>>();
+    GetInOut.getInOut(D, Ins, Outs);
+
+/*
+    System.out.println("Outs");
+    for(ArrayList<Integer> link : Outs){
+      int linkIndex = Outs.indexOf(link);
+      for(int adj : link){
+        System.out.println(linkIndex+" --> "+adj);
+      }
+    }
+
+    System.out.println("Ins");
+    for(ArrayList<Integer> link : Ins){
+      int linkIndex = Ins.indexOf(link);
+      for(int adj : link){
+        System.out.println(linkIndex+" <-- "+adj);
+      }
+    }
+*/
+//__________________________________________________________________________________________________________
+  //x PDA(w, N^in_D, N^out_D, eps)
+  // Price-Directive Algorithm
+    double eps = 0.15;
+    System.out.println("PDA");
+    PDA.run(eps, links, Ins, Outs);
+/*
+    for(Link link : links){
+      int a = links.indexOf(link);
+      double xNoutD = 0.0;
+      for(int adj : Outs.get(a)){ 
+        xNoutD = xNoutD + links.get(adj).x;
+      }
+      //System.out.println("x(NoutD["+links.indexOf(link)+"])= "+xNoutD);
+
+      double xNinD = 0.0;
+      for(int adj : Ins.get(a)){ 
+        xNinD = xNinD + links.get(adj).x;
+      }
+      //System.out.println("x(NinD["+links.indexOf(link)+"])= "+sum);
+      if(xNinD >= xNoutD){
+        System.out.println("x-surplus link= "+a);
+      }
+    }
+*/
+/*
+    for(Link link : links){
+      int a = links.indexOf(link);
+      System.out.println("x("+a+")= "+link.x);
+    }
+*/
+//__________________________________________________________________________________________________________
+  //I PG(w, A, x)
+  // Prune-and-Grow algorithm
+    System.out.println("PG");
+    System.out.println("I = " + PG.run(eps, links, Ins, Outs, G));
+
+
+//__________________________________________________________________________________________________________
+    System.out.println("END");
+  }
+}
---
+++ src/PG.java
@@ -0,0 +1,133 @@
+import java.util.*;
+
+public class PG{
+
+  private static boolean IS(Set<Integer> C, Grafo G){
+    //return true if the set is independent, false otherwise
+    //check pairwise if the solution is an independent set 
+    //(no edge between the selected nodes)
+
+    for(int a : C){
+      for(int b : C){
+        if(b>a){
+          //System.out.println(a+" "+b);
+          //System.out.println(" adj:"+G.adjacencia.get(a).contains(b));
+          if(G.adjacencia.get(a).contains(b)) return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  public static Set<Integer> run(double eps, ArrayList<Link> links,
+                          ArrayList<ArrayList<Integer>> Ins,
+                          ArrayList<ArrayList<Integer>> Outs,
+                          Grafo G){
+    int i=0, a;
+
+    ArrayList<Link> B_links = new ArrayList<Link>(links);
+    ArrayList<ArrayList<Integer>> B_Ins = new ArrayList<ArrayList<Integer>>(Ins);
+    ArrayList<ArrayList<Integer>> B_Outs = new ArrayList<ArrayList<Integer>>(Outs);
+
+   
+    System.out.println("  Prune-Phase");
+      //B ← A, S ← ∅; //S is a stack
+      Set<Integer> B = new TreeSet<Integer>();
+      for (i=0;i<links.size();i++){
+        B.addAll(Arrays.asList(links.get(i).id));
+      }
+
+    Stack<Integer> S = new Stack<Integer>();
+      while(!B.isEmpty()){
+        //System.out.println("=================================");
+
+        //a ← a x-surplus link of B;                
+        a = Link.get_x_surplus(links, B, Ins, Outs);
+        //System.out.println("a= "+a);
+
+        //B ← B ∖ {a};
+        B.removeAll(Arrays.asList(a));
+        //System.out.println(B);
+          
+
+          //**************************************************************************
+          // I do not understand why this part of the code makes exist x-surplus
+          // My initial idea was to be able to execute
+                //PDA.run(eps, B_links, B_Ins, B_Outs);
+          // TODO: dedicate time to understand...        
+          //**************************************************************************
+            B_links.remove(links.get(a));
+            //System.out.print("B_links [");
+            //for(Link l : B_links)
+            //  System.out.print(l.id+", ");
+            //System.out.println("]");
+            B_Ins.remove(Ins.get(a));
+            for(ArrayList<Integer> bins : B_Ins){ 
+              if(bins.contains(a)) bins.remove(bins.indexOf(a));
+            }
+            //System.out.println("B_Ins "+B_Ins);
+            B_Outs.remove(Outs.get(a));
+            for(ArrayList<Integer> bouts : B_Outs){ 
+              if(bouts.contains(a)) bouts.remove(bouts.indexOf(a));
+            }
+            //System.out.println("B_Outs "+B_Outs);
+          //**************************************************************************
+
+
+        //w_bar(a) ← w (a) − w_bar (S ∩ N (a));
+        double sum_weight_bar = 0.0;
+        for(int adj : Outs.get(a)){
+          if(S.search(adj)>0){
+            sum_weight_bar = sum_weight_bar + links.get(adj).weight_bar;
+          }
+        }
+        for(int adj : Ins.get(a)){ 
+          if(S.search(adj)>0){
+            sum_weight_bar = sum_weight_bar + links.get(adj).weight_bar;
+          }
+        }
+        links.get(a).set_weight_bar(links.get(a).weight - sum_weight_bar);
+
+        //if w(a) > 0, push a onto S;
+        if(links.get(a).weight_bar > 0.0) S.push(new Integer(a));
+        //System.out.println("weight_bar("+a+")= "+links.get(a).weight_bar);
+
+      }
+
+    System.out.println("  Grow-Phase");
+      //I ← ∅;
+      Set<Integer> I = new TreeSet<Integer>();
+
+                  Stack<Integer> S_reverse = new Stack<Integer>();
+                  while (!S.empty()) {
+                    S_reverse.push(S.pop());
+                  }
+                  //System.out.println(S_reverse);
+                  //System.out.println(S);
+
+      //while S ∕= ∅,
+      while(!S_reverse.empty()){
+        //System.out.println("=================================");
+        //pop the top link a from S; 
+        a = S_reverse.pop();
+        //System.out.println("pop "+a);
+
+        //if I ∪ {a} is independent, I ← I ∪ {a};
+        Set<Integer> Sa = new TreeSet<Integer>();
+        Sa.add(a);
+        Set<Integer> union = new TreeSet<Integer>();
+        union.addAll(I);
+        union.addAll(Sa);
+
+        //System.out.println("set_candidate: "+union);
+        if(IS(union, G)) {
+          I.clear();
+          I.addAll(union);
+        }
+      }
+
+    //return I.    
+    return I;
+  }
+
+}
