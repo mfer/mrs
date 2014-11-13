61782b8de42146c3257218ae0c6f94e98d7ab437
mfer <sudolshw@gmail.com>
2014-11-13 09:49:13 -0200

    bug when  calling PDA twice.

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,14 +1,30 @@
 import java.io.*;
 import java.util.*;
-
 /*
 *   call me like that:
-*   make simulation FILENAME=wolfram nLinks=10 minLength=0.1 maxLength=1.0 minZ=0.1 maxZ=1.0 escala=1.0
+*   make simulation FILENAME=random nLinks=10 minLength=0.1 maxLength=1.0 minZ=0.1 maxZ=1.0 escala=1.0
 */
 
-public class Simulation{
+public class Simulation {
+  private static Set<Integer> run(String[] args, double eps)  throws FileNotFoundException, IOException{
+    Grafo G = InstanceReader.readInstance(args[0]);
+    //GrafoRadial.draw(G.links, true);
+    Grafo D = Orientate.edge(G);
+    ArrayList<ArrayList<Integer>> Ins = new ArrayList<ArrayList<Integer>>();
+    ArrayList<ArrayList<Integer>> Outs = new ArrayList<ArrayList<Integer>>();
+    GetInOut.getInOut(D, Ins, Outs);
 
-  public static void main(String[] args) throws FileNotFoundException, IOException {
+    System.out.println("prePDA");
+    PDA.run(eps, G.links, Ins, Outs);
+    System.out.println("posPDA");
+
+    return PG.run(eps, G.links, Ins, Outs, G);
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) throws FileNotFoundException, IOException{
     if(args.length<1){
       System.out.println("Usage:");
       System.out.println("  java Simulation <filename>");
@@ -17,18 +33,35 @@ public class Simulation{
       System.exit(0);
     }
 
-    Grafo G = InstanceReader.readInstance(args[0]);
-    //GrafoRadial.draw(G.links, true);
+    double eps = 0.5;
+    int numRuns = 2;
 
-    Grafo D = Orientate.edge(G);
+    String logFile = "log_file.txt";
+    PrintStream log;
+    try {
+      log = new PrintStream(logFile);
+      log.println("START"); log.println();
 
-    ArrayList<ArrayList<Integer>> Ins = new ArrayList<ArrayList<Integer>>();
-    ArrayList<ArrayList<Integer>> Outs = new ArrayList<ArrayList<Integer>>();
-    GetInOut.getInOut(D, Ins, Outs);
+        DataSeries ds = new DataSeries();
+        int r;
+        for (r = 0; r < numRuns; r++)
+        {
+            ds.addSample(r);
+            Set<Integer> sol = run(args, eps);
+            System.out.println(sol.size());
+            System.out.println(sol);
+        }
 
-    double eps = 0.15;
-    PDA.run(eps, G.links, Ins, Outs);
+      log.println("END");
+      log.println("MEAN, VAR, STD: "+ds.getMean()+", "+
+        ds.getVariance()+", "+ds.getStandardDeviation());
 
-    System.out.println("I = " + PG.run(eps, G.links, Ins, Outs, G));
+
+
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+
   }
 }
